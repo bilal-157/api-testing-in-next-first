b4961eecc3f84b4cd1f940d4a33d89f88ca4d2a0
Bilal Rafique <rafiqueb087@gmail.com>
2025-07-05 09:42:51 +0500

    second commit

--- app/api/hello/route.js
+++
@@ -1,9 +0,0 @@
-// Correct format
-export async function GET(request) {
-  return Response.json({ message: 'Hello from API!' });
-}
-
-export async function POST(request) {
-  const data = await request.json();
-  return Response.json({ received: data });
-}
